a6767927982a7653a99e974ff32eccfdf7795353
x74z <166341818+x74z@users.noreply.github.com>
2024-11-11 18:59:53 +0000

    Add content to html, next is adding it from js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // import imageName from "./path/to/image";
 import "./styles.css";
+import pigeonEatingBread from "../assets/pigeon-eating-bread.jpg";
 
 
-
-
+const image = document.createElement("img")
+image.src = pigeonEatingBread;
 console.log("works? yup");
+document.querySelector("body").appendChild(image)
